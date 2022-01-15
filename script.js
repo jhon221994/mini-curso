5aae804321b857c216fc9ae4a879554d4518b590
junior morais <juniormorais180180@gmail.com>
2022-01-15 11:51:21 -0300

    capturar elementos html de manipulaçao

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especieDoPersonagem = document.querySelector('#especie');
+const condiçaoDoPersonagem = document.querySelector('#condiçao');
