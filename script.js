8ec6508f4e703d7a8c2a4673f10e38769a4b772e
junior morais <juniormorais180180@gmail.com>
2022-01-15 13:42:25 -0300

    conexao entre o java e o api da imagem

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especieDoPersonagem = document.querySelector('#especie');
-const condiçaoDoPersonagem = document.querySelector('#condiçao');
+const condiçaoDoPersonagem = document.querySelector('#condicao');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET', {
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+        }
+    }).then((response) => response.json()).then((data)) => {
+        imagem.src = data.image;
+    });
+}
+
+botao.onclick = pegarPersonagem;
