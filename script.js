7eaf15cf5b713edc4d40701d3624b0a7b731b977
junior morais <juniormorais180180@gmail.com>
2022-01-15 20:38:05 -0300

    emplementar id no html

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const imagem = document.querySelector('img');
+const imagemDois = document.querySelector('#img2');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condiçao = document.querySelector('#condicao');
+const nomeDoPersonagemDois = document.queryselector('#nome2');
+
+
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
@@ -25,4 +29,24 @@ pegarPersonagem = () => {
     });
 }
 
-botao.onclick = pegarPersonagem;
+pegarPersonagemDois = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+    }).then((response) => response.json()).then((data) => {
+        imagemDois.src = data.image;
+        imagemDois.alt = data.name;
+        nomeDoPersonagemDois.innerHTML = data.name;
+
+    });
+}
+
+mostrarPersonagem = () => {
+    pegarPersonagem();pegarPersonagemDois();
+}
+
+
+botao.onclick = mostrarPersonagem;
