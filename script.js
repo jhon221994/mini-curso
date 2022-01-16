05b5042b0138b41aae7cd217a0d4b6b946c8ca4d
junior morais <juniormorais180180@gmail.com>
2022-01-16 12:08:27 -0300

    erro na linha 86 do script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const imagem = document.querySelector('img');
 const imagemDois = document.querySelector('#imgm');
 const imagemTres = document.queryselector('#imgmm');
-const botao = document.querySelector('button');
+const botaoo = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const nomePersonagemDois = document.querySelector('#nomme');
 const nomePersonagemTres = document.queryselector('#nommme');
@@ -35,7 +35,7 @@ pegarPersonagem = () => {
     });
 }
 
-erarValorAleatorio = () => {
+gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
@@ -51,13 +51,36 @@ pegarPersonagemDois = () => {
         imagemDois.src = data.image;
         imagemDois.alt = data.name;
         nomePersonagemDois.innerHTML = data.name;
+        especieDois.innerHTML = data.species;
+        condicaoDois.innerHTML = data.status;
+
+    });
+}
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+
+
+pegarPersonagemTres = () => {
+    let numeroAleatorioTres = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioTres}`, {
+        method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+    }).then((response) => response.json()).then((data) => {
+        imagemTres.src = data.image;
+        imagemTres.alt = data.name;
+        nomePersonagemTres.innerHTML = data.name;
+        especieTres.innerHTML = data.species;
+        condicaoTres.innerHTML = data.status;
 
     });
 }
 
 mostrarPersonagem = () => {
-    pegarPersonagem();pegarPersonagemDois();
+    pegarPersonagem();pegarPersonagemDois();pegarPersonagemTres();
 }
 
-
-botao.onclick = mostrarPersonagem;
+botaoo.onclick = mostrarPersonagem;
